9468320544953ecce2cb7549e497b345617c3dc6
JadBGHIEL <jad.bghiel@epitech.eu>
2026-04-24 16:34:54 +0200

    add header for json parsing

---
+++ include/parser/JsonHelper.hpp
@@ -0,0 +1,27 @@
+/*
+** EPITECH PROJECT, 2026
+** JsonHelper
+** File description:
+** JsonHelper
+*/
+#pragma once
+
+#include <cstddef>
+#include <string>
+#include "SceneParser.hpp"
+
+namespace RayTracer {
+namespace Parser {
+
+class JsonHelper {
+public:
+    static bool tryExtractJsonNumber(const std::string &content, std::size_t &index, double &value, SceneParseError &error);
+    static bool tryExtractJsonObject(const std::string &content, std::size_t &index, std::string &objectStr, SceneParseError &error);
+    static std::size_t skipWhitespace(const std::string &content, std::size_t index);
+
+private:
+    static bool isWhitespace(char c);
+};
+
+}
+}
