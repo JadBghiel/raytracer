eda0947c3de233ecaaa59d618060d226b7553ff7
JadBGHIEL <jad.bghiel@epitech.eu>
2026-04-24 14:37:02 +0200

    Add: primitive facotry sample TODO

---
+++ include/PrimitiveFactory.hpp
@@ -0,0 +1,22 @@
+/*
+** EPITECH PROJECT, 2026
+** PrimitiveFactory
+** File description:
+** PrimitiveFactory
+*/
+#pragma once
+
+#include <memory>
+#include "IPrimitive.hpp"
+
+namespace RayTracer {
+
+class PrimitiveFactory {
+public:
+    virtual ~PrimitiveFactory() = default;
+
+    // factory methods will be added as real primitives are implemented TODO
+    // placehodler here to estbalius the pattern TODO
+};
+
+}
