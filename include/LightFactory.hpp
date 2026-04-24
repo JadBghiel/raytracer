2f3320e8453eb821f12a8b7a8da71922c929b05c
JadBGHIEL <jad.bghiel@epitech.eu>
2026-04-24 14:37:19 +0200

    Add:light factory sample TODO

---
+++ include/LightFactory.hpp
@@ -0,0 +1,22 @@
+/*
+** EPITECH PROJECT, 2026
+** LightFactory
+** File description:
+** LightFactory
+*/
+#pragma once
+
+#include <memory>
+#include "ILight.hpp"
+
+namespace RayTracer {
+
+class LightFactory {
+public:
+    virtual ~LightFactory() = default;
+
+    // factory methods will be added as the real lights are implemented
+    // placeholder to establish the pattern
+};
+
+}
