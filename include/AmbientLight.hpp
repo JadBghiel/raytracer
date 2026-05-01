3a99b48411e13f9eb651c10b3bad6eb5373d162b
JadBGHIEL <jad.bghiel@epitech.eu>
2026-05-01 16:11:37 +0200

    add: ambient light header

---
+++ include/AmbientLight.hpp
@@ -0,0 +1,31 @@
+/*
+** EPITECH PROJECT, 2026
+** AmbientLight
+** File description:
+** AmbientLight
+*/
+#pragma once
+
+#include "ILight.hpp"
+
+namespace RayTracer {
+
+class AmbientLight : public ILight {
+private:
+    double _intensity;
+
+public:
+    explicit AmbientLight(double intensity) : _intensity(intensity) {}
+    ~AmbientLight() = default;
+
+    Math::Vector3 contribute(const Math::Point3 &point, const Math::Vector3 &normal) const override
+    {
+        (void)point;
+        (void)normal;
+        return Math::Vector3(_intensity, _intensity, _intensity);
+    }
+
+    double intensity() const { return _intensity; }
+};
+
+}
