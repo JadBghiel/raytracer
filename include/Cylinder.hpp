ef3824a1ddbb1fc7a9065133c0a8b983adb6b69d
Nicolas <183367651+Nvcoo@users.noreply.github.com>
2026-05-01 19:04:06 +0200

    add: cylinder header file

---
+++ include/Cylinder.hpp
@@ -0,0 +1,30 @@
+/*
+** EPITECH PROJECT, 2026
+** Cylinder
+** File description:
+** Cylinder
+*/
+
+#ifndef INCLUDED_CYLINDER_HPP
+    #define INCLUDED_CYLINDER_HPP
+
+#include "Color.hpp"
+#include "IPrimitive.hpp"
+#include "Math.hpp"
+
+namespace RayTracer {
+class Cylinder : public IPrimitive
+{
+    private:
+        Math::Point3 _center;
+        double _radius;
+        double _height;
+        Color _color;
+    public:
+        Cylinder(const Math::Point3 &center, double radius, double height, const Color &color) : _center(center), _radius(radius), _height(height), _color(color) {};
+        ~Cylinder() = default;
+        HitRecord intersect(const Ray &ray, double tMin, double tMax) const override;
+};
+}
+
+#endif
