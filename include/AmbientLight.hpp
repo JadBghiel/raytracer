f3796f7832a40702f88efdf1e855a3a3e48efd12
Nicolas <183367651+Nvcoo@users.noreply.github.com>
2026-05-10 18:58:26 +0200

    add: primitive parameter to the light

--- include/AmbientLight.hpp
+++ include/AmbientLight.hpp
@@ -18,10 +18,11 @@ public:
     explicit AmbientLight(double intensity) : _intensity(intensity) {}
     ~AmbientLight() = default;
 
-    Math::Vector3 contribute(const Math::Point3 &point, const Math::Vector3 &normal) const override
+    Math::Vector3 contribute(const Math::Point3 &point, const Math::Vector3 &normal, const std::vector<std::shared_ptr<IPrimitive>> &primitives) const override
     {
         (void)point;
         (void)normal;
+        (void)primitives;
         return Math::Vector3(_intensity, _intensity, _intensity);
     }
 
