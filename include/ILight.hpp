06534befda26d1476c539a128faa15b30393589d
JadBGHIEL <jad.bghiel@epitech.eu>
2026-05-11 17:29:45 +0200

    rm: dupe header

--- include/ILight.hpp
+++ include/ILight.hpp
@@ -9,8 +9,6 @@
 #include <memory>
 #include <vector>
 #include "Math.hpp"
-#include <memory>
-#include <vector>
 
 namespace RayTracer {
 
@@ -19,11 +17,9 @@ class IPrimitive;
 class ILight {
 public:
     virtual ~ILight() = default;
-// compute light contribution at the given point with the given normal
-    // returns a vector representing the light intensity/color contribution
-    // primitives are passed here so lights can cast shadow rays
-    // and check if any object blocks the path to the light source
-    virtual Math::Vector3 contribute(const Math::Point3 &point, const Math::Vector3 &normal, const std::vector<std::shared_ptr<IPrimitive>> &primitives) const = 0;
+
+    virtual Math::Vector3 contribute(const Math::Point3 &point, const Math::Vector3 &normal,
+        const std::vector<std::shared_ptr<IPrimitive>> &primitives) const = 0;
 };
 
 }
