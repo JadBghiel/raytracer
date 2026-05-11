6aaa7157cd9038a15e994371bda3c5a3f4b049ae
JadBGHIEL <jad.bghiel@epitech.eu>
2026-05-11 17:11:23 +0200

    add: pirm as param in the light interface

--- include/ILight.hpp
+++ include/ILight.hpp
@@ -6,17 +6,20 @@
 */
 #pragma once
 
+#include <memory>
+#include <vector>
 #include "Math.hpp"
+#include "IPrimitive.hpp"
 
 namespace RayTracer {
 
 class ILight {
 public:
     virtual ~ILight() = default;
-
-    // compute light contribution at the given point with the given normal
+// compute light contribution at the given point with the given normal
     // returns a vector representing the light intensity/color contribution
-    virtual Math::Vector3 contribute(const Math::Point3 &point, const Math::Vector3 &normal) const = 0;
+    virtual Math::Vector3 contribute(const Math::Point3 &point, const Math::Vector3 &normal,
+        const std::vector<std::shared_ptr<IPrimitive>> &primitives) const = 0;
 };
 
 }
